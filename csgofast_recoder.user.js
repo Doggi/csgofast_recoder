096b12c1f29527269d7f4ffa75bf5a2a1d2e5c06
Sören Grzanna <soeren.grzanna@stud.fh-hannover.de>
2016-02-09 21:39:01 +0100

    Initial commit

---
+++ csgofast_recoder.user.js
@@ -0,0 +1,7 @@
+// ==UserScript==
+// @name        csgofast_recoder
+// @namespace   de.grzanna-online.ogame
+// @include     https://csgofast.com/
+// @version     1
+// @grant       none
+// ==/UserScript==
